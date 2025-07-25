8307791a4d848606ee08dd1ab0f8ddb7f87b49a7
CorlinF <166881173+CorlinF@users.noreply.github.com>
2025-07-24 19:19:56 -0700

    Update geo_helpers.dart

--- campus_tour/lib/helpers/geo_helpers.dart
+++ campus_tour/lib/helpers/geo_helpers.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'package:vector_math/vector_math_64.dart';
 
 // Calculate distance between two points using Haversine formula
 double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
@@ -18,20 +17,4 @@ double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
 
 double toRadians(double degrees) {
   return degrees * (pi / 180);
-}
-
-double computeBearing(double lat1, double lon1, double lat2, double lon2) {
-  final dLon = (lon2 - lon1) * (pi / 180);
-
-  final y = sin(dLon) * cos(toRadians(lat2));
-  final x = 
-  cos(toRadians(lat1)) * sin(toRadians(lat2)) -
-  sin(toRadians(lat1)) * cos(toRadians(lat2)) * cos(dLon);
-  return atan2(y, x);
-}
-
-Vector3 bearingToDirection(double bearingRadians) {
-  final x = sin(bearingRadians);
-  final z = cos(bearingRadians);
-  return Vector3(x, 0, z).normalized();
 }
